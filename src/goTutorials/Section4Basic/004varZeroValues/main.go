3c3a3892622e7617cba58a1eaa5dfe1594c6769a
YuxinZhang <taylorzhangyx@gmail.com>
2020-05-07 13:21:02 -0700

    value and zero values" "
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

---
+++ src/goTutorials/Section4Basic/004varZeroValues/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+	// int, string, bool, float64 are basic types in go
+	// Other basic types are:
+	/*
+	bool
+
+	string
+
+	int  int8  int16  int32  int64
+	uint uint8 uint16 uint32 uint64 uintptr
+
+	byte 	// alias for uint8
+
+	rune 	// alias for int32
+			// represents a Unicode code point
+
+	float32 float64
+
+	complex64 complex128
+	*/
+	var value = 123;
+
+	fmt.Println(value)
+	fmt.Printf("The type of value is: %T\n", value)
+
+	var vS = "test string";
+
+	fmt.Println(vS)
+	fmt.Printf("The type of value is: %T\n", vS)
+
+	var vF = 123.22;
+
+	fmt.Println(vF)
+	fmt.Printf("The type of value is: %T\n", vF)
+
+	var vB = true
+
+	fmt.Println(vB)
+	fmt.Printf("The type of value is: %T\n", vB)
+}
