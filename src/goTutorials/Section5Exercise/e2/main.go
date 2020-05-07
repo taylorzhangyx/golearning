7a11582bc2846871771ed6e6ebb08b56a93b3493
YuxinZhang <taylorzhangyx@gmail.com>
2020-05-07 15:53:45 -0700

    exercise2
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

--- src/goTutorials/Section5Exercise/e2/main.go
+++ src/goTutorials/Section5Exercise/e2/main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+// package level declaration
+// they will be assigned to zero values if no initialization values are provided.
+var x int
+var y string
+var z bool
+
 func main(){
-	
+	fmt.Println(x) //0
+	fmt.Println(y) //""
+	fmt.Println(z) //false
 }
