1ddb3ee57d3f30723ddd776bedc553cf1604ab93
YuxinZhang <taylorzhangyx@gmail.com>
2020-05-11 14:33:56 -0700

    add code to print the matrix
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

--- src/goTutorials/Section8ControlFlow/fors/main.go
+++ src/goTutorials/Section8ControlFlow/fors/main.go
@@ -1,11 +1,21 @@
 package main
+
 import (
 	"fmt"
 )
 
-
-func main(){
+func main() {
 	// for loops
 	// print out the matrix of x,y and the values
-	
+	x, y := 10, 8
+	drawMatrix(x, y)
+}
+
+func drawMatrix(x int, y int) {
+	for i := 0; i < x; i++ {
+		for j := 0; j < y; j++ {
+			fmt.Printf("[%v,%v]\t", i, j)
+		}
+		fmt.Printf("\n")
+	}
 }
