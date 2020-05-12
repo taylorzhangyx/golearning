30a5bc29eaee1128d4b6292224bbe1aab32f7b4d
YuxinZhang <taylorzhangyx@gmail.com>
2020-05-11 19:37:18 -0700

    ary and slicing
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

--- src/goTutorials/Section9Grouping/main.go
+++ src/goTutorials/Section9Grouping/main.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	defineAry()
+	makeaSlice()
+}
+
+func defineAry() {
 	var ary [40]int
 	for i := 0; i <= 100; i++ {
 		ary[i%40] = i
@@ -10,4 +15,20 @@ func main() {
 	fmt.Println(ary)
 
 	fmt.Printf("type of ary is: %T", ary) // the length is included as a part of type
+}
+
+func makeaSlice() {
+
+	slc := make([]int, 0)
+	fmt.Println(slc)
+
+	for i := 0; i < 10; i++ {
+		slc = append(slc, i)
+	}
+	fmt.Println(slc)
+
+	for i, v := range slc {
+		fmt.Println(i, v)
+	}
+
 }
