aa1c9f0820cd4b19b0c697c78531733e17fedfb1
YuxinZhang <taylorzhangyx@gmail.com>
2020-05-13 18:08:52 -0700

    e10, delete k,v pair in dict
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

--- src/goTutorials/Section10Exercises/slice/main.go
+++ src/goTutorials/Section10Exercises/slice/main.go
@@ -107,3 +107,20 @@ func e9(dict map[string][]string) {
 		fmt.Println(k, v)
 	}
 }
+func e10() {
+	m := map[string][]string{
+		`bond_james`:      {`Shaken, not stirred`, `Martinis`, `Women`},
+		`moneypenny_miss`: {`James Bond`, `Literature`, `Computer Science`},
+		`no_dr`:           {`Being evil`, `Ice cream`, `Sunsets`},
+	}
+	m[`fleming_ian`] = []string{`steaks`, `cigars`, `espionage`}
+
+	delete(m, `no_dr`)
+
+	for k, v := range m {
+		fmt.Println("This is the record for", k)
+		for i, v2 := range v {
+			fmt.Println("\t", i, v2)
+		}
+	}
+}
