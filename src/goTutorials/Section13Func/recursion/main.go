5d33745204838da61110d35b8ffbc81fb4d3eeb4
YuxinZhang <taylorzhangyx@gmail.com>
2020-05-14 14:53:18 -0700

    use recursion, closure, and anonumous functions
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

--- src/goTutorials/Section13Func/recursion/main.go
+++ src/goTutorials/Section13Func/recursion/main.go
@@ -1,21 +1,42 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fib := initFib()
-	for i := 0; i < 10; i++ {
-		v := fib() // return the first 10 fib values
-		fmt.Println(i, v)
-	}
+	// fib := initFib()
+	// for i := 0; i < 10; i++ {
+	// 	v := fib() // return the first 10 fib values
+	// 	fmt.Println(i, v)
+	// }
+
+	recurFib := reverseFib(20)
+	recurFib()
 }
 
+// closure way to print out the fib numbers
 func initFib() func() int {
 	a, b := 1, 1
 	return func() int {
 		a, b = b, a+b
 		return a
 	}
+}
+
+func reverseFib(count int) func() {
+	a, b := 1, 1
+	return func() {
+		a, b = fib(a, b, count)
+	}
+}
+
+func fib(a int, b int, n int) (int, int) {
+	a, b = b, a+b
+	if n != 0 {
+		n--
+		fmt.Println(n, a)
+		fib(a, b, n)
+	} else {
+		fmt.Println("DONE")
+	}
+	return a, b
 }
