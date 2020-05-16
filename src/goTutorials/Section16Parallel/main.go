9f66fec87efceeaade44cf31bae9d21da617eda9
YuxinZhang <taylorzhangyx@gmail.com>
2020-05-15 23:46:40 -0700

    mutex
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

--- src/goTutorials/Section16Parallel/main.go
+++ src/goTutorials/Section16Parallel/main.go
@@ -71,3 +71,32 @@ func racecondition() {
 	wg.Wait()
 	fmt.Printf("DONE, %v", count)
 }
+
+func mutex() {
+	var m sync.Mutex
+	var wg sync.WaitGroup
+	exe := func(n int) {
+		notuse := 0
+		for i := 0; i < 9999; i++ {
+			notuse += i
+		}
+		fmt.Println("Done", n)
+		wg.Done()
+	}
+
+	count := 0
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
+		go func(i int) {
+			m.Lock()
+			counter := count
+
+			runtime.Gosched()
+			go exe(i)
+			count = counter + 1
+			m.Unlock()
+		}(i)
+	}
+	wg.Wait()
+	fmt.Printf("DONE, %v", count)
+}
