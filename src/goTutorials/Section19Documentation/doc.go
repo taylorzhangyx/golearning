de2bd2a779161fdb36ab82048cca5d603f9c0965
YuxinZhang <taylorzhangyx@gmail.com>
2020-05-18 19:09:05 -0700

    add doc examples
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

---
+++ src/goTutorials/Section19Documentation/doc.go
@@ -0,0 +1,18 @@
+// Package doc The package to demonstrate how the documentation works on GO
+package doc
+
+import (
+	"fmt"
+)
+
+// Show Print out the documentation for Show.
+func Show() {
+	fmt.Println("Showing the documentation for doc package")
+
+}
+
+// WriteDown Save the doc in the default file.
+func WriteDown() {
+	fmt.Println("Write down the documentation")
+}
+
