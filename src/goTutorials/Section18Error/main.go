99b1c2ade4c936f3ff56444b350fa347d014e928
YuxinZhang <taylorzhangyx@gmail.com>
2020-05-18 17:47:47 -0700

    add panic and recover examples
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

--- src/goTutorials/Section18Error/main.go
+++ src/goTutorials/Section18Error/main.go
@@ -11,5 +11,27 @@ import (
 // https://blog.golang.org/defer-panic-and-recover
 
 func main() {
-	fmt.Println("Hello, playground")
+	f()
+	fmt.Println("Returned normally from f.")
+}
+
+func f() {
+	defer func() {
+		if r := recover(); r != nil { // Revocer is only useful in deferred function
+			fmt.Println("Recovered in f", r)
+		}
+	}()
+	fmt.Println("Calling g.")
+	g(0)
+	fmt.Println("Returned normally from g.") // This line will not be executed since the panic popped up the function
+}
+
+func g(i int) {
+	if i > 3 {
+		fmt.Println("Panicking!")
+		panic(fmt.Sprintf("%v", i))
+	}
+	defer fmt.Println("Defer in g", i)
+	fmt.Println("Printing in g", i)
+	g(i + 1)
 }
