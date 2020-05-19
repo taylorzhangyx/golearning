10218298b2b9f5ce97ed8b29e06f5a20f0432f96
YuxinZhang <taylorzhangyx@gmail.com>
2020-05-18 17:35:53 -0700

    error handleing
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

---
+++ src/goTutorials/Section18Error/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+// type error interface {
+// 	Error() string
+// }
+
+// https://blog.golang.org/defer-panic-and-recover
+
+func main() {
+	fmt.Println("Hello, playground")
+}
