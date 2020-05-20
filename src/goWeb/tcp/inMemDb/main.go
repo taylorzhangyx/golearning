7e258fefc605917d5a2f55162a56b94c02a3cf9b
YuxinZhang <taylorzhangyx@gmail.com>
2020-05-19 20:28:20 -0700

    implement the in mem db
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

--- src/goWeb/tcp/inMemDb/main.go
+++ src/goWeb/tcp/inMemDb/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"net"
+	"strings"
 )
 
 func main() {
@@ -28,11 +29,44 @@ func main() {
 
 func memDb(connection net.Conn) {
 	scner := bufio.NewScanner(connection)
+	db := make(map[string]string)
 	for scner.Scan() {
 		line := scner.Text()
-		log.Println(line)
-		io.WriteString(connection, fmt.Sprintln("Yo!", line))
+		log.Println("command:", line, connection.LocalAddr(), connection.RemoteAddr())
+
+		command, data := split(line)
+
+		res := work(&db, command, data)
+
+		io.WriteString(connection, fmt.Sprintln("Db is working:", res))
 	}
-	defer log.Println("TIME OUT!")
 	connection.Close()
+}
+
+func work(db *map[string]string, cmd string, data string) string {
+	var res string
+
+	tokens := strings.Split(data, " ")
+	key := tokens[0]
+	val := strings.Join(tokens[1:], " ")
+
+	switch cmd {
+	case "ADD":
+		(*db)[key] = val
+		res = fmt.Sprintln(key, ":", val, "is saved")
+	case "DEL":
+		delete(*db, key)
+		res = fmt.Sprintln(key, "is deleted")
+	case "GET":
+		res = (*db)[key]
+	default:
+		res = "Invalid! Try again."
+	}
+	return res
+}
+
+func split(line string) (string, string) {
+	tokens := strings.Split(line, " ")
+	data := strings.Join(tokens[1:], " ")
+	return tokens[0], data
 }
