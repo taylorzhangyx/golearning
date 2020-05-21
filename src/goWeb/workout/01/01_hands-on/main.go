9317b4aee9619e4435694ab03671abcc0a4033f0
YuxinZhang <taylorzhangyx@gmail.com>
2020-05-20 20:16:26 -0700

    res of e1
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

---
+++ src/goWeb/workout/01/01_hands-on/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Hello, playground")
+	http.HandleFunc("/", def)
+	http.HandleFunc("/dog/", dog)
+	http.HandleFunc("/me/", me)
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func def(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "default")
+}
+
+func dog(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "doggy")
+}
+
+func me(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Yuxin Zhang")
+
+}
